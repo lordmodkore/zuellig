b5f1a1dee861d2b013447c28746017d42734c787
elland <elland@mediamerchants.com.au>
2023-12-31 13:23:04 +0800

    justtify left partner logos

--- wp-content/themes/zuellig/resources/views/front-page.blade.php
+++ wp-content/themes/zuellig/resources/views/front-page.blade.php
@@ -292,9 +292,9 @@
                                 @while(have_rows('resource_partners'))
                                     @php the_row() @endphp
                                     <div class="col-lg-3 col-md-3 col-sm-6 py-2 my-4">
-                                        <div class="partner-logo-container" style="background-image: url({!! get_sub_field('resource_partners_logo') !!})">
-{{--                                            <img src="{!! get_sub_field('resource_partners_logo') !!}" alt=""--}}
-{{--                                                 class="img-fluid partner-logo">--}}
+                                        <div>
+                                            <img src="{!! get_sub_field('resource_partners_logo') !!}" alt=""
+                                                 class="img-fluid partner-logo">
                                         </div>
                                     </div>
                                 @endwhile
