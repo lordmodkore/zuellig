0399227c057e0c26b002c0df1e7727dd41810d96
elland <elland@mediamerchants.com.au>
2024-01-02 09:46:00 +0800

    Learnings Page template

--- wp-content/themes/zuellig/resources/views/partials/content-page-our-stories.blade.php
+++ wp-content/themes/zuellig/resources/views/partials/content-page-our-stories.blade.php
@@ -142,8 +142,8 @@
                                         @endphp
                                         {!! wp_trim_words( $content, 20 ); !!}
                                     </p>
-                                    <a href="{!! get_the_permalink($featuredPost[0]) !!}" class="d-none text-decoration-none text-white link_default-value">
-                                        <div class="d-fle">
+                                    <a href="{!! get_the_permalink($featuredPost[0]) !!}" class="text-decoration-none text-white link_default-value">
+                                        <div class="d-flex">
                                             <p>READ MORE<span>&nbsp
                                                 <i class="fa fa-chevron-right"></i></span></p>
                                         </div>
