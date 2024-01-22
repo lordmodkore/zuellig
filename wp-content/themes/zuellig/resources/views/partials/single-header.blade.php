bd1d9bb25b9649fd2f4c2637206c078c93e9d589
elland <elland@mediamerchants.com.au>
2024-01-22 20:47:26 +0800

    fix post thumb nail

--- wp-content/themes/zuellig/resources/views/partials/single-header.blade.php
+++ wp-content/themes/zuellig/resources/views/partials/single-header.blade.php
@@ -3,7 +3,4 @@
 	 $image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full');
 @endphp
 <div class="position-relative">
-    <div class="featured-image-container">
-        {!! get_the_post_thumbnail(get_the_ID() ,'feature-image-thumb', array( 'class' => 'card-img-top h-100 img-fluid mb-5' ) ); !!}
-    </div>
 </div>
