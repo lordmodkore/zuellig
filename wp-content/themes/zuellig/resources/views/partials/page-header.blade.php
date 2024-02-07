511fc02fb8c57d4af3d749c54a88f34a0e7d916c
elland <elland@mediamerchants.com.au>
2024-02-07 11:50:13 +0800

    Dont show banner if not assigned

--- wp-content/themes/zuellig/resources/views/partials/page-header.blade.php
+++ wp-content/themes/zuellig/resources/views/partials/page-header.blade.php
@@ -1,6 +1,8 @@
 @php
     $postId = get_queried_object_id();
 @endphp
-<div class="position-relative">
-    <img src="{!! get_field('page_header_banner') !!}" alt="banner" class="img-fluid w-100">
-</div>
+@if(get_field('page_header_banner'))
+    <div class="position-relative">
+        <img src="{!! get_field('page_header_banner') !!}" alt="banner" class="img-fluid w-100">
+    </div>
+@endif
